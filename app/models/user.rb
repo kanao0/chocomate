ed0ceed8bb2297dad071040ca60cd860a3f90370
KanaOyaizu <143867420+kanao0@users.noreply.github.com>
2023-11-09 16:35:47 +0000

    [create]ゲストログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # ユーザー1人につき1つのプロフィール画像
+  has_one_attached :profile_image
+
+  # ゲストユーザーのメールアドレス
+  GUEST_USER_EMAIL = "guest@example.com"
+
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+      user.bio = "これはゲストユーザーです"
+    end
+  end
+
+
+
+  # プロフィールイメージがないとき
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,64 +1,63 @@
 Rails.application.routes.draw do
-  
-  root to: 'user/homes#top'  
-  
-  namespace :user do
+
+  root to: 'user/homes#top'
+
+  scope module: :user do
     get 'users/mypage' => 'users#show'
     get 'users/quit' => 'users#quit'
-    patch 'users/withdraw' => 'users#withdraw'    
-    resources :users, only: [:edit, :update]  
-    resources :reviews  
+    patch 'users/withdraw' => 'users#withdraw'
+    resources :users, only: [:edit, :update]
   end
-  
-  namespace :user do
-    get 'users/mypage' => 'users#show'
-    get 'users/quit' => 'users#quit'
-    patch 'users/withdraw' => 'users#withdraw'    
-    resources :users, only: [:edit, :update]  
-    resources :reviews  
-  end
-  
-  namespace :admin do
-    get 'homes/top'
-  end
-  
-  namespace :admin do
-    resources :origins, only: [:index, :create, :edit, :update, :destroy]
-  end
-  
-  namespace :admin do
-    resources :types, only: [:index, :create, :edit, :update, :destroy]   
-  end
-  
-  namespace :admin do
-    resources :brands, only: [:index, :create, :edit, :update, :destroy]   
-  end
-  
-  namespace :admin do
-    resources :products, only: [:index, :create, :edit, :update, :destroy]     
+
+  scope module: :user do
+    resources :reviews
   end
 
   namespace :admin do
-    resources :reviews, only: [:index, :show, :edit, :update, :destroy]       
+    get 'homes/top'
+  end
+
+  namespace :admin do
+    resources :origins, only: [:index, :create, :edit, :update, :destroy]
+  end
+
+  namespace :admin do
+    resources :types, only: [:index, :create, :edit, :update, :destroy]
+  end
+
+  namespace :admin do
+    resources :brands, only: [:index, :create, :edit, :update, :destroy]
+  end
+
+  namespace :admin do
+    resources :products, only: [:index, :create, :edit, :update, :destroy]
+  end
+
+  namespace :admin do
+    resources :reviews, only: [:index, :show, :edit, :update, :destroy]
     get 'reviews/index'
     get 'reviews/show'
     get 'reviews/edit'
   end
 
   namespace :admin do
-    resources :users, only: [:index, :show, :edit, :update]     
+    resources :users, only: [:index, :show, :edit, :update]
   end
-    
-  
-  
-  
+
+
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "user/registrations",
     sessions: 'user/sessions'
   }
-  
+
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
-  }  
-  
+  }
+
+
+  devise_scope :user do
+    post "user/guest_sign_in", to: "user/sessions#guest_sign_in"
+  end
+
+
 end
