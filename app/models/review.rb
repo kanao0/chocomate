518d299731646ad8be8a488f2da49dca435e185c
KanaOyaizu <143867420+kanao0@users.noreply.github.com>
2024-01-07 13:14:53 +0000

    review.rb更新

--- app/models/review.rb
+++ app/models/review.rb
@@ -54,7 +54,7 @@ class Review < ApplicationRecord
   def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
     end
     image.variant(gravity: :center, resize:"#{width}x#{height}^", crop:"#{width}x#{height}+0+0").processed
   end
